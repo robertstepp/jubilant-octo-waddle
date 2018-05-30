f363ade4c5a11312f793c583a07ebe8c57027112
Robert Stepp <robert@vyperinc.ninja>
2018-05-30 14:32:46 -0700

    astr

--- src/MEvsVE.java
+++ src/MEvsVE.java
@@ -1,20 +1,20 @@
 // Java Program by Robert Stepp 05/30/2018
 /**
  * 
- * @author Robert Stepp 
- * @title Lab 10 Comprehensive 
- * @description Write a program to solve the
- *         ìVegetarians and Meat Eatersî problem. Three vegetarians and three
- *         hungry meat-eaters need to cross a river. Unfortunately, the boat
- *         only holds two people. If the meat-eaters outnumber the vegetarians
- *         on either bank, the vegetarians will be eaten! Please note: nobody
- *         gets to stay on the boat. When thereís three meat-eaters on one side,
- *         it doesnít matter if one just came over on the boat or not. Your
- *         challenge is to find a series of moves that gets all three
- *         vegetarians and all three meat-eaters across the river safely. Write
- *         a program that solves the vegetarians and meat-eaters problem. You
- *         are given broad leeway on how to represent this problem. Recursion
- *         and backtracking (depth-first search) is one way to solve this.
+ * @author Robert Stepp
+ * @title Lab 10 Comprehensive
+ * @description Write a program to solve the ‚ÄúVegetarians and Meat Eaters‚Äù
+ *              problem. Three vegetarians and three hungry meat-eaters need to
+ *              cross a river. Unfortunately, the boat only holds two people. If
+ *              the meat-eaters outnumber the vegetarians on either bank, the
+ *              vegetarians will be eaten! Please note: nobody gets to stay on
+ *              the boat. When there‚Äôs three meat-eaters on one side, it doesn‚Äôt
+ *              matter if one just came over on the boat or not. Your challenge
+ *              is to find a series of moves that gets all three vegetarians and
+ *              all three meat-eaters across the river safely. Write a program
+ *              that solves the vegetarians and meat-eaters problem. You are
+ *              given broad leeway on how to represent this problem. Recursion
+ *              and backtracking (depth-first search) is one way to solve this.
  *
  * 
  */
@@ -35,6 +35,7 @@ class builder {
 	public String getName() {
 		return this.name;
 	}
+
 	public String getType() {
 		return this.type;
 	}
