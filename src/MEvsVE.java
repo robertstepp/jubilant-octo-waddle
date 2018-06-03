24d613eeb444d7d0ef0e822efbe5abdb67bcdadd
Robert Stepp <robert@vyperinc.ninja>
2018-06-02 21:12:20 -0700

    Attempted findMoveNorth

--- src/MEvsVE.java
+++ src/MEvsVE.java
@@ -4,6 +4,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 import javax.swing.JOptionPane;
 
@@ -32,35 +33,22 @@ public class MEvsVE {
 	static ArrayList<String> testCases = new ArrayList<String>();
 	static Map<String, Integer> tempForTest = new HashMap<String, Integer>();
 	static Map<String, Integer> tempParent = new HashMap<String, Integer>();
+	static Map<String, Integer> tempPreviousParent = new HashMap<String, Integer>();
 	static ArrayList<String> testCasesReciprocol = new ArrayList<String>();
 	static PrintWriter output;
-	static HashMap<String, Integer> south = new HashMap<String, Integer>() { // Start
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-
-		{
-			put("Meat Eaters", 3);
-			put("Vegetarians", 3);
-		}
-	};
-	static HashMap<String, Integer> north = new HashMap<String, Integer>() { // End
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-
-		{
-			put("Meat Eaters", 0);
-			put("Vegetarians", 0);
-		}
-	};
+	static char direction = 'n';
+	static TreeNode<String> root = new TreeNode<String>();
 
 	public static void fillReciprocol() {
-		for (int r = 0; r < testCases.size(); r++) {
-			testCasesReciprocol.add(testCases.get(testCases.size() - (r + 1)));
+		Stack<String> reversal = new Stack<String>();
+		for (int i = 0; i < testCases.size(); i++) {
+			reversal.push(testCases.get(i));
 		}
+		int size = reversal.size();
+		for (int j = 0; j < size; j++) {
+			testCasesReciprocol.add((String) reversal.pop());
+		}
+		removeDisparities();
 	}
 
 	public static void buildTestCases(int total) {
@@ -69,10 +57,18 @@ public class MEvsVE {
 				testCases.add("M" + i + "V" + j);
 			}
 		}
-		for (int l = 0; l < testCases.size(); l++) { // This is what will parse the test case to test against the next values.
-			parseTestCases(testCases.get(l));
-		}
 		fillReciprocol();
+	}
+
+	public static void removeDisparities() {
+		for (int i = 0; i < testCases.size(); i++) {
+			parseTestCases(testCases.get(i));
+			if (tempForTest.get("M") > tempForTest.get("V")) {
+				int position = testCases.indexOf(testCases.get(i));
+				testCases.remove(position);
+				testCasesReciprocol.remove(position);
+			}
+		}
 	}
 
 	public static void parseTestCases(String testCase) {
@@ -82,15 +78,59 @@ public class MEvsVE {
 	}
 
 	public static void startTree(int total) {
-		TreeNode<String> root = new TreeNode<String>();
 		root.data = "M" + total + "V" + total;
-		findNext(root.data);
+		findMoveNorth(root);
 	}
 
-	public static void findNext(String parent) {
-		String[] temp = parent.split("");
+	public static void findMoveNorth(TreeNode<String> parent) {
+		String[] temp = ((String) parent.data).split("");
 		tempParent.put(temp[0], Integer.parseInt(temp[1]));
 		tempParent.put(temp[2], Integer.parseInt(temp[3]));
+		int parentTotal = tempParent.get("M") + tempParent.get("V");
+		for (int tC = 0; tC < testCases.size(); tC++) {
+			if (testCases.get(tC) != parent.data) {
+				parseTestCases(testCases.get(tC));
+				int testCaseTotal = tempForTest.get("M") + tempForTest.get("V");
+				int difference = parentTotal - testCaseTotal;
+				if (tempForTest.get("M") <= tempForTest.get("V") && difference <= 2 && difference > 0 && direction == 'n') {
+					TreeNode<String> child = parent.addChild(testCases.get(tC));
+					// System.out.println(testCases.get(tC));
+					direction = 's';
+					findMoveNorth(child);
+				} else if (tempForTest.get("V") != 0 && difference < 0 && direction == 's') {
+					if (tempForTest.get("M") <= tempForTest.get("V") && difference >= -2) {
+						TreeNode<String> child = parent.addChild(testCasesReciprocol.get(tC));
+						direction = 'n';
+						findMoveNorth(child);
+					}
+				}
+			} else if (testCases.get(tC) == parent.data) {
+				System.out.println("Failure");
+			}
+
+		}
+
+	}
+
+	public static <T> void outputTree(TreeNode<T> t) {
+
+		// if (t.getParent() == null)
+		// System.out.print("Root node: ");
+
+		// This recurses repeatedly through the parents of each branch of the
+		// tree
+		// This is O(n^2) and could be dramatically improved.
+		TreeNode<T> parent = t.getParent();
+		// System.out.print("\"");
+		while (parent != null) {
+			System.out.print("-");
+			parent = parent.getParent();
+		}
+		System.out.println(t.data);
+		// System.out.println(t.data + "\"");
+
+		for (TreeNode<T> a : t.getChildren())
+			outputTree(a);
 	}
 
 	public static void openFile() throws IOException {
@@ -115,5 +155,7 @@ public class MEvsVE {
 		buildTestCases(input);
 		startTree(input);
 		closeFile();
+		outputTree(root);
 	}
+
 }
