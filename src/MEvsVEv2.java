bc512a7651111b6d422545c8d00a61142122ded7
Robert Stepp <robert@vyperinc.ninja>
2018-06-03 09:55:36 -0700

    Update Javadoc

--- src/MEvsVEv2.java
+++ src/MEvsVEv2.java
@@ -1,3 +1,24 @@
+// Java Program by Robert Stepp 05/30/2018
+/**
+ * 
+ * @author Robert Stepp
+ * @title Lab 10 Comprehensive
+ * @description Write a program to solve the"Vegetarians and Meat Eaters"
+ *              problem. Three vegetarians and three hungry meat-eaters need to
+ *              cross a river. Unfortunately, the boat only holds two people. If
+ *              the meat-eaters outnumber the vegetarians on either bank, the
+ *              vegetarians will be eaten! Please note: nobody gets to stay on
+ *              the boat. When there's three meat-eaters on one side, it doesn't
+ *              matter if one just came over on the boat or not. Your challenge
+ *              is to find a series of moves that gets all three vegetarians and
+ *              all three meat-eaters across the river safely. Write a program
+ *              that solves the vegetarians and meat-eaters problem. You are
+ *              given broad leeway on how to represent this problem. Recursion
+ *              and backtracking (depth-first search) is one way to solve this.
+ *
+ * 
+ */
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
